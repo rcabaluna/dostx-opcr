be10913dc7174eb51286706884bba034b99c58f4
Ruel Cabaluna Jr <rcabalunajr@gmail.com>
2024-03-05 12:04:45 +0800

    Added Registry Controller and Views

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -22,16 +22,9 @@ class Dashboard extends BaseController
         return view('registry', $data);
     }
 
-<<<<<<< HEAD
-    public function add_registry()
-    {
-        $data['title'] = "Registry";
-        return view('add-registry', $data);
-=======
     public function Profile()
     {
         $data['title'] = "profile";
         return view('profile', $data);
->>>>>>> 442241d8e1a39ef01a1c983ab3cecf95514687d5
     }
 }
