defaab26f79a7310b57205c948391ff3c255bdde
Francis <franiscl.dionaldo@gmail.com>
2024-03-05 11:59:09 +0800

    update

--- app/Views/registry.php
+++ app/Views/registry.php
@@ -8,19 +8,13 @@
           <div class="row align-items-end mb-3">
             <div class="col-sm mb-2 mb-sm-0">
               <nav aria-label="breadcrumb">
-                <ol class="breadcrumb breadcrumb-no-gutter">
-                  <li class="breadcrumb-item"><a class="breadcrumb-link" href="javascript:;">Pages</a></li>
-                  <li class="breadcrumb-item"><a class="breadcrumb-link" href="javascript:;">Project</a></li>
-                  <li class="breadcrumb-item active" aria-current="page">Teams</li>
-                </ol>
               </nav>
 
-              <h1 class="page-header-title">Teams</h1>
             </div>
 
             <div class="col-sm-auto">
               <a class="btn btn-primary" href="javascript:;" data-toggle="modal" data-target="#shareWithPeopleModal">
-                <i class="tio-add mr-1"></i> Add team
+                <i class="tio-add mr-1"></i> New User
               </a>
             </div>
           </div>
@@ -266,13 +260,7 @@
               <tbody>
                 
                     <div class="hs-unfold">
-                      <a class="js-hs-unfold-invoker btn btn-sm btn-white" href="javascript:;"
-                         data-hs-unfold-options='{
-                           "target": "#teamsDropdown7",
-                           "type": "css-animation"
-                         }'>
-                        More <i class="tio-chevron-down ml-1"></i>
-                      </a>
+    
 
                       <div id="teamsDropdown7" class="hs-unfold-content dropdown-unfold dropdown-menu dropdown-menu-sm dropdown-menu-right">
                         <a class="dropdown-item" href="#"></a>
@@ -302,31 +290,10 @@
                       <label class="custom-control-label" for="teamDataCheck9"></label>
                     </div>
                   </td>
-                  <td class="table-column-pl-0"><a href="#">#sales</a></td>
-                  <td>Managing a sales team is no easy task. You have the potential to either make or break your sales reps.</td>
-                  <td><a class="badge badge-soft-primary p-2" href="#">Sales</a></td>
-                  <td>
-                    <div class="d-flex">
-                      <div class="mr-1"><img src="assets/svg/components/star.svg" alt="Review rating" width="14"></div>
-                      <div class="mr-1"><img src="assets/svg/components/star.svg" alt="Review rating" width="14"></div>
-                      <div class="mr-1"><img src="assets/svg/components/star.svg" alt="Review rating" width="14"></div>
-                      <div class="mr-1"><img src="assets/svg/components/star.svg" alt="Review rating" width="14"></div>
-                      <div class="mr-1"><img src="assets/svg/components/star.svg" alt="Review rating" width="14"></div>
-                    </div>
-                  </td>
-                  <td>
-                    <div class="avatar-group avatar-group-xs avatar-circle">
-                      <span class="avatar" data-toggle="tooltip" data-placement="top" title="Ella Lauda">
-                        <img class="avatar-img" src="assets/img/160x160/img9.jpg" alt="Image Description">
-                      </span>
-                      <span class="avatar" data-toggle="tooltip" data-placement="top" title="David Harrison">
-                        <img class="avatar-img" src="assets/img/160x160/img3.jpg" alt="Image Description">
-                      </span>
-                      <span class="avatar" data-toggle="tooltip" data-placement="top" title="Finch Hoot">
-                        <img class="avatar-img" src="assets/img/160x160/img5.jpg" alt="Image Description">
-                      </span>
-                    </div>
-                  </td>
+                  <td class="table-column-pl-0"><a href="#">Totoy Tum</a></td>
+                  <td>totoytumalonka@email.com</td>
+                  <td><a class="badge badge-soft-primary p-2" href="#">November 1, 2002</a></td>
+                
                   <td>
                     <div class="hs-unfold">
                       <a class="js-hs-unfold-invoker btn btn-sm btn-white" href="javascript:;"
@@ -338,9 +305,7 @@
                       </a>
 
                       <div id="teamsDropdown9" class="hs-unfold-content dropdown-unfold dropdown-menu dropdown-menu-sm dropdown-menu-right">
-                        <a class="dropdown-item" href="#">Rename team</a>
-                        <a class="dropdown-item" href="#">Add to favorites</a>
-                        <a class="dropdown-item" href="#">Archive team</a>
+                        <a class="dropdown-item" href="#">Edit</a>
                         <div class="dropdown-divider"></div>
                         <a class="dropdown-item text-danger" href="#">Delete</a>
                       </div>
@@ -351,49 +316,26 @@
                 <tr>
                   <td class="table-column-pr-0">
                     <div class="custom-control custom-checkbox">
-                      <input type="checkbox" class="custom-control-input" id="teamDataCheck10">
-                      <label class="custom-control-label" for="teamDataCheck10"></label>
+                      <input type="checkbox" class="custom-control-input" id="teamDataCheck9">
+                      <label class="custom-control-label" for="teamDataCheck9"></label>
                     </div>
                   </td>
-                  <td class="table-column-pl-0"><a href="#">#webdev</a></td>
-                  <td>Web talents</td>
-                  <td><a class="badge badge-soft-dark p-2" href="#">Development</a></td>
-                  <td>
-                    <div class="d-flex">
-                      <div class="mr-1"><img src="assets/svg/components/star.svg" alt="Review rating" width="14"></div>
-                      <div class="mr-1"><img src="assets/svg/components/star.svg" alt="Review rating" width="14"></div>
-                      <div class="mr-1"><img src="assets/svg/components/star.svg" alt="Review rating" width="14"></div>
-                      <div class="mr-1"><img src="assets/svg/components/star.svg" alt="Review rating" width="14"></div>
-                      <div class="mr-1"><img src="assets/svg/components/star.svg" alt="Review rating" width="14"></div>
-                    </div>
-                  </td>
-                  <td>
-                    <div class="avatar-group avatar-group-xs avatar-circle">
-                      <span class="avatar avatar-soft-danger" data-toggle="tooltip" data-placement="top" title="Teresa Eyker">
-                        <span class="avatar-initials">T</span>
-                      </span>
-                      <span class="avatar" data-toggle="tooltip" data-placement="top" title="David Harrison">
-                        <img class="avatar-img" src="assets/img/160x160/img3.jpg" alt="Image Description">
-                      </span>
-                      <span class="avatar avatar-soft-warning" data-toggle="tooltip" data-placement="top" title="Olivier L.">
-                        <span class="avatar-initials">O</span>
-                      </span>
-                    </div>
-                  </td>
+                  <td class="table-column-pl-0"><a href="#">Totoy Tum</a></td>
+                  <td>totoytumalonka@email.com</td>
+                  <td><a class="badge badge-soft-primary p-2" href="#">November 1, 2002</a></td>
+                
                   <td>
                     <div class="hs-unfold">
                       <a class="js-hs-unfold-invoker btn btn-sm btn-white" href="javascript:;"
                          data-hs-unfold-options='{
-                           "target": "#teamsDropdown10",
+                           "target": "#teamsDropdown9",
                            "type": "css-animation"
                          }'>
                         More <i class="tio-chevron-down ml-1"></i>
                       </a>
 
-                      <div id="teamsDropdown10" class="hs-unfold-content dropdown-unfold dropdown-menu dropdown-menu-sm dropdown-menu-right">
-                        <a class="dropdown-item" href="#">Rename team</a>
-                        <a class="dropdown-item" href="#">Add to favorites</a>
-                        <a class="dropdown-item" href="#">Archive team</a>
+                      <div id="teamsDropdown9" class="hs-unfold-content dropdown-unfold dropdown-menu dropdown-menu-sm dropdown-menu-right">
+                        <a class="dropdown-item" href="#">Edit</a>
                         <div class="dropdown-divider"></div>
                         <a class="dropdown-item text-danger" href="#">Delete</a>
                       </div>
@@ -404,46 +346,26 @@
                 <tr>
                   <td class="table-column-pr-0">
                     <div class="custom-control custom-checkbox">
-                      <input type="checkbox" class="custom-control-input" id="teamDataCheck11">
-                      <label class="custom-control-label" for="teamDataCheck11"></label>
+                      <input type="checkbox" class="custom-control-input" id="teamDataCheck9">
+                      <label class="custom-control-label" for="teamDataCheck9"></label>
                     </div>
                   </td>
-                  <td class="table-column-pl-0"><a href="#">#socialteam</a></td>
-                  <td>Team that manages and runs socials accounts of the company.</td>
-                  <td><a class="badge badge-soft-info p-2" href="#">Marketing team</a></td>
-                  <td>
-                    <div class="d-flex">
-                      <div class="mr-1"><img src="assets/svg/components/star.svg" alt="Review rating" width="14"></div>
-                      <div class="mr-1"><img src="assets/svg/components/star.svg" alt="Review rating" width="14"></div>
-                      <div class="mr-1"><img src="assets/svg/components/star.svg" alt="Review rating" width="14"></div>
-                      <div class="mr-1"><img src="assets/svg/components/star-muted.svg" alt="Review rating" width="14"></div>
-                      <div class="mr-1"><img src="assets/svg/components/star-muted.svg" alt="Review rating" width="14"></div>
-                    </div>
-                  </td>
-                  <td>
-                    <div class="avatar-group avatar-group-xs avatar-circle">
-                      <span class="avatar" data-toggle="tooltip" data-placement="top" title="Costa Quinn">
-                        <img class="avatar-img" src="assets/img/160x160/img6.jpg" alt="Image Description">
-                      </span>
-                      <span class="avatar avatar-soft-dark" data-toggle="tooltip" data-placement="top" title="Zack Ins">
-                        <span class="avatar-initials">Z</span>
-                      </span>
-                    </div>
-                  </td>
+                  <td class="table-column-pl-0"><a href="#">Totoy Tum</a></td>
+                  <td>totoytumalonka@email.com</td>
+                  <td><a class="badge badge-soft-primary p-2" href="#">November 1, 2002</a></td>
+                
                   <td>
                     <div class="hs-unfold">
                       <a class="js-hs-unfold-invoker btn btn-sm btn-white" href="javascript:;"
                          data-hs-unfold-options='{
-                           "target": "#teamsDropdown11",
+                           "target": "#teamsDropdown9",
                            "type": "css-animation"
                          }'>
                         More <i class="tio-chevron-down ml-1"></i>
                       </a>
 
-                      <div id="teamsDropdown11" class="hs-unfold-content dropdown-unfold dropdown-menu dropdown-menu-sm dropdown-menu-right">
-                        <a class="dropdown-item" href="#">Rename team</a>
-                        <a class="dropdown-item" href="#">Add to favorites</a>
-                        <a class="dropdown-item" href="#">Archive team</a>
+                      <div id="teamsDropdown9" class="hs-unfold-content dropdown-unfold dropdown-menu dropdown-menu-sm dropdown-menu-right">
+                        <a class="dropdown-item" href="#">Edit</a>
                         <div class="dropdown-divider"></div>
                         <a class="dropdown-item text-danger" href="#">Delete</a>
                       </div>
@@ -451,67 +373,11 @@
                   </td>
                 </tr>
 
-                <tr>
-                  <td class="table-column-pr-0">
-                    <div class="custom-control custom-checkbox">
-                      <input type="checkbox" class="custom-control-input" id="teamDataCheck12">
-                      <label class="custom-control-label" for="teamDataCheck12"></label>
-                    </div>
-                  </td>
-                  <td class="table-column-pl-0"><a href="#">#design</a></td>
-                  <td>Creative minds</td>
-                  <td><a class="badge badge-soft-primary p-2" href="#">Design</a></td>
-                  <td>
-                    <div class="d-flex">
-                      <div class="mr-1"><img src="assets/svg/components/star.svg" alt="Review rating" width="14"></div>
-                      <div class="mr-1"><img src="assets/svg/components/star.svg" alt="Review rating" width="14"></div>
-                      <div class="mr-1"><img src="assets/svg/components/star.svg" alt="Review rating" width="14"></div>
-                      <div class="mr-1"><img src="assets/svg/components/star.svg" alt="Review rating" width="14"></div>
-                      <div class="mr-1"><img src="assets/svg/components/star.svg" alt="Review rating" width="14"></div>
-                    </div>
-                  </td>
-                  <td>
-                    <div class="avatar-group avatar-group-xs avatar-circle">
-                      <span class="avatar" data-toggle="tooltip" data-placement="top" title="Ella Lauda">
-                        <img class="avatar-img" src="assets/img/160x160/img9.jpg" alt="Image Description">
-                      </span>
-                      <span class="avatar" data-toggle="tooltip" data-placement="top" title="David Harrison">
-                        <img class="avatar-img" src="assets/img/160x160/img3.jpg" alt="Image Description">
-                      </span>
-                      <span class="avatar avatar-soft-dark" data-toggle="tooltip" data-placement="top" title="Antony Taylor">
-                        <span class="avatar-initials">A</span>
-                      </span>
-                      <span class="avatar avatar-soft-info" data-toggle="tooltip" data-placement="top" title="Sara Iwens">
-                        <span class="avatar-initials">S</span>
-                      </span>
-                      <span class="avatar" data-toggle="tooltip" data-placement="top" title="Finch Hoot">
-                        <img class="avatar-img" src="assets/img/160x160/img5.jpg" alt="Image Description">
-                      </span>
-                      <span class="avatar avatar-light avatar-circle" data-toggle="tooltip" data-placement="top" title="Sam Kart, Amanda Harvey and 1 more">
-                        <span class="avatar-initials">+3</span>
-                      </span>
-                    </div>
-                  </td>
-                  <td>
-                    <div class="hs-unfold">
-                      <a class="js-hs-unfold-invoker btn btn-sm btn-white" href="javascript:;"
-                         data-hs-unfold-options='{
-                           "target": "#teamsDropdown12",
-                           "type": "css-animation"
-                         }'>
-                        More <i class="tio-chevron-down ml-1"></i>
-                      </a>
+              
 
-                      <div id="teamsDropdown12" class="hs-unfold-content dropdown-unfold dropdown-menu dropdown-menu-sm dropdown-menu-right">
-                        <a class="dropdown-item" href="#">Rename team</a>
-                        <a class="dropdown-item" href="#">Add to favorites</a>
-                        <a class="dropdown-item" href="#">Archive team</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item text-danger" href="#">Delete</a>
-                      </div>
-                    </div>
-                  </td>
-                </tr>
+                
+
+              
               </tbody>
             </table>
           </div>
